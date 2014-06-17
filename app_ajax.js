a920a4b1b0c2ab1d8eed0f949add7eac6d5dc4f4
Ganesh Iyer <lastlegion@gmail.com>
2014-06-18 03:14:31 +0530

    Using dataset of million records
    Former-commit-id: 5ed22e9f9474e90d2923130c2ad460b071a1753e

--- app_ajax.js
+++ app_ajax.js
@@ -37,44 +37,55 @@ for(var attribute in schema){
 }
 //console.log(visual_attributes)
 // Read the CSV file into flights
-var dataraw = fs.readFileSync("big_data.json");
+var dataraw = fs.readFileSync("mil_data.json");
+//var dataraw = fs.readFileSync("250_data.json")
 data = JSON.parse(dataraw)
 
 
 var ndx = crossfilter(data),
   dimensions = {
-
     Ai: ndx.dimension(function (d){return d.Ai;}),
     Bi: ndx.dimension(function (d){return d.Bi;}),
     Ci: ndx.dimension(function (d){return d.Ci;}),
     Di: ndx.dimension(function (d){return d.Di;}),
-    Eb: ndx.dimension(function (d){return d.Ei}),
-    Ff: ndx.dimension(function (d){return d.Ff}),
+    age: ndx.dimension(function (d){return d.age;}),
+    gender: ndx.dimension(function (d){return d.gender;}),
+    isActive: ndx.dimension(function (d) {return d.isActive;}),
     Gf: ndx.dimension(function (d){return d.Gf}),
     Hf: ndx.dimension(function (d){return d.Hf})
   },
   groups = {
-
     Ai: dimensions.Ai.group(),
     Bi: dimensions.Bi.group(),
     Ci: dimensions.Ci.group(),
     Di: dimensions.Di.group(),
-    Eb: dimensions.Eb.group(),
-    Ff: dimensions.Ff.group(function (d){return Math.round(d*10)/10}),
-    Gf: dimensions.Gf.group(),
-    Hf: dimensions.Hf.group()
-  },
+    age: dimensions.age.group(),
+    gender: dimensions.gender.group(),
+    isActive: dimensions.isActive.group(),
+    Gf: dimensions.Gf.group(function(d){return Math.round(d*10)/10;}),
+    Hf: dimensions.Hf.group(function(d){return Math.round(d*10)/10;})
+  }
   size = ndx.size(),
   all = ndx.groupAll();
 
 // Handle the AJAX requests
 app.use("/data",function(req,res,next) {
+  
+  console.log("Getting /data")
   filter = req.param("filter") ? JSON.parse(req.param("filter")) : {}
   // Loop through each dimension and check if user requested a filter
 
   // Assemble group results and and the maximum value for each group
   var results = {} 
+  var filter_dim;
+  var filter_range=[];
+  //console.log(filter[dim])
+  for(var key in filter){
+    filter_dim= key;
+  }
+  
   Object.keys(dimensions).forEach(function (dim) {
+
     if (filter[dim]) {
       // In this example the only string variables in the filter are dates
       //console.log("here")
@@ -92,14 +103,19 @@ app.use("/data",function(req,res,next) {
       }
 
       dimensions[dim].filterRange(filter[dim])
-      results["table_data"] = {data: dimensions[dim].top(100)}
     } else {
-      dimensions[dim].filterAll()
-
-      	results["table_data"] = {data: dimensions[dim].top(100)}
+      dimensions[dim].filterAll(null)
     }
   })
-
+  
+  if(Object.keys(filter).length === 0){
+      //dimensions["Ai"].filter(null)
+      results["table_data"] = {data:dimensions["Ai"].top(100)}
+  }
+  else{
+      //dimensions[filter_dim].filterRange(filter_range)
+      results["table_data"] = {data:dimensions[filter_dim]  .top(100)}
+  }
   Object.keys(groups).forEach(function(key) {
       results[key] = {values:groups[key].all(),top:groups[key].top(1)[0].value}
   })
