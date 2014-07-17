463beadda3d4806ec0170d99847ff72f9154f092
Ganesh Iyer <lastlegion@gmail.com>
2014-07-17 11:32:18 +0530

    added data-source.json. Schema for describing data sources
    Former-commit-id: e82cf326016679d2c09fe354439e56dc803b3af7

--- app_ajax.js
+++ app_ajax.js
@@ -7,7 +7,7 @@ var http = require('http');
 var path = require('path');
 var crossfilter = require("./crossfilter.v1.min.js").crossfilter;
 var fs = require('fs');
-
+var d3 = require('d3');
 var app = express();
 
 // all environments
@@ -26,34 +26,86 @@ app.use(express.static(path.join(__dirname, 'public')));
 var visual_attributes = [];
 var filtering_attributes = [];
 
-//Read the schema
-var schema = fs.readFileSync("public/backend-schema.json");
-schema = JSON.parse(schema);
-//console.log(schema)
-for(var attribute in schema){
-	console.log(attribute)
-	if(schema[attribute]["visual-attribute"])
-		visual_attributes.push(schema[attribute]["name"]);
-  if(schema[attribute]["filtering-attribute"])
-    filtering_attributes.push(schema[attribute]["name"]);
+var data_sources = [];
+
+
+function load_data()
+{
+  console.log("here ");
+  console.log(data_sources)
+  for(var data_source in data_sources){
+    if(data_sources[data_source].type == "json"){
+      var dataraw = fs.readFileSync(data_sources[data_source].path);
+      data = JSON.parse(dataraw);
+      console.log()
+    }
+  }
 }
-//console.log(visual_attributes)
-// Read the CSV file into flights
+
+function process_data_source(){
+  var data_source_schema = fs.readFileSync("public/data-source.json");
+  data_source_schema = JSON.parse(data_source_schema);
+
+
+  for(var data_source in data_source_schema){
+    console.log(data_source_schema[data_source]);
+    data_sources.push(data_source_schema[data_source]);
+    //Join Logic?
+
+  }
+
+  console.log(data_sources);
+
+  load_data();
+
+}
+
+var dimensions = {};
+var groups = {};
+var ndx;
+function apply_crossfilter(){
+  ndx = crossfilter(data);
+  for(var attr in filtering_attributes){
+    console.log(filtering_attributes[attr])
+    dimension = ndx.dimension(function(d){return d[filtering_attributes[attr]]});
+    dimensions[filtering_attributes[attr]] = dimension;
+    group = dimension.group();
+    groups[filtering_attributes[attr]] = group;
+  }
+    size = ndx.size(),
+    all = ndx.groupAll(); 
+}
+
+function process_backend_schema(){
+  //Read the schema
+  var schema = fs.readFileSync("public/backend-schema.json");
+  schema = JSON.parse(schema);
+  //console.log(schema)
+  for(var attribute in schema){
+    console.log(attribute)
+    if(schema[attribute]["visual-attribute"])
+      visual_attributes.push(schema[attribute]["name"]);
+    if(schema[attribute]["filtering-attribute"])
+      filtering_attributes.push(schema[attribute]["name"]);
+  }
+
+  apply_crossfilter();
+
+}
+
+
+process_data_source();
+process_backend_schema();
+
+
+
+
+/*
 var dataraw = fs.readFileSync("data/small-data.json");
 //var dataraw = fs.readFileSync("250_data.json")
 data = JSON.parse(dataraw)
-var dimensions = {};
-var groups = {};
-var ndx = crossfilter(data);
-for(var attr in filtering_attributes){
-  console.log(filtering_attributes[attr])
-  dimension = ndx.dimension(function(d){return d[filtering_attributes[attr]]});
-  dimensions[filtering_attributes[attr]] = dimension;
-  group = dimension.group();
-  groups[filtering_attributes[attr]] = group;
-}
-  size = ndx.size(),
-  all = ndx.groupAll();
+*/
+
 
 // Handle the AJAX requests
 app.use("/data",function(req,res,next) {
@@ -72,14 +124,8 @@ app.use("/data",function(req,res,next) {
   }
   
   Object.keys(dimensions).forEach(function (dim) {
-
     if (filter[dim]) {
-      // In this example the only string variables in the filter are dates
-      //console.log("here")
-      //console.log(dim)
       console.log(filter[dim])
-    
-      
       dimensions[dim].filter(filter[dim])
     } else {
       dimensions[dim].filterAll(null)
@@ -87,7 +133,6 @@ app.use("/data",function(req,res,next) {
   })
   
   if(Object.keys(filter).length === 0){
-      //dimensions["Ai"].filter(null)
       results["table_data"] = {data:dimensions["Ai"].top(100)}
   }
   else{
@@ -97,8 +142,7 @@ app.use("/data",function(req,res,next) {
   Object.keys(groups).forEach(function(key) {
       results[key] = {values:groups[key].all(),top:groups[key].top(1)[0].value}
   })
-  //console.log(results)
-  //console.log(dimensions["age"].top(100))  
+
   // Send back as json
   res.writeHead(200, { 'content-type': 'application/json' });
   res.end((JSON.stringify(results)))
