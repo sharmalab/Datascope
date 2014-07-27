76a3cc299404cccc55535f4ab3256bee5d8843d1
Ganesh Iyer <lastlegion@gmail.com>
2014-07-27 20:45:49 +0530

    Updated validation script
    Former-commit-id: 925cf3a3e940f3749f356dece6bc4c9f8deaabd4

--- validate.js
+++ validate.js
@@ -1,13 +1,13 @@
 var Validator = require('jsonschema').Validator;
 var v = new Validator()
 var fs = require("fs");
-var data_description = JSON.parse(fs.readFileSync("public/schemas/data-description.json"));
-var interactive_filters = JSON.parse(fs.readFileSync("public/schemas/interactive-filters.json"));
-var data_source = JSON.parse(fs.readFileSync("public/schemas/data-source.json"))
+var data_description = JSON.parse(fs.readFileSync("public/schemas/dataDescription.json"));
+var interactive_filters = JSON.parse(fs.readFileSync("public/schemas/interactiveFilters.json"));
+var data_source = JSON.parse(fs.readFileSync("public/schemas/dataSource.json"))
 
-var data_description_schema = JSON.parse(fs.readFileSync("schemas/data-description.json"));
-var interactive_filters_schema = JSON.parse(fs.readFileSync("schemas/interactive-filters.json"));
-var data_source_schema = JSON.parse(fs.readFileSync("schemas/data-source.json"));
+var data_description_schema = JSON.parse(fs.readFileSync("schemas/dataDescriptionSchema.json"));
+var interactive_filters_schema = JSON.parse(fs.readFileSync("schemas/interactiveFiltersSchema.json"));
+var data_source_schema = JSON.parse(fs.readFileSync("schemas/dataSourceSchema.json"));
 
 var res1 = v.validate(data_description, data_description_schema);
 var res2 = v.validate(interactive_filters, interactive_filters_schema);
@@ -20,6 +20,8 @@ else
 	console.log(res1.errors)
 if(res2.errors.length == 0)
 	console.log("interactive_filters_schema passed");
+else 
+	console.log(res2.errors)
 if(res3.errors.length == 0)
 	console.log("data_source_schema passed"); 
 else
