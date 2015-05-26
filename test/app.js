bba36102aa2b31a5adcf9dda1f5d6a9eec7f5b7e
Ganesh Iyer <lastlegion@gmail.com>
2015-05-26 12:37:31 +0530

    test: app.js
    Former-commit-id: aa66eb74418dc9c77e2d063ade61b84e26f2cb16

--- test/app.js
+++ test/app.js
@@ -2,22 +2,38 @@ var assert = require("assert");
 var http = require("http");
 var request = require("superagent");
 var expect = require("expect.js");
+
+var main = require("../main.js");
+
 describe("app", function() {
 
 	describe("GET /", function() {
-		it("responds with the app", function() {
-			request.get("http://localhost:3000/", function(res){
-				expect(res).to.exist;
-				expect(res.status).to.equal(200);
+
+
+		it("responds with the app", function(done) {
+		
+
+			main.init(function(){
+				request.get("http://localhost:3000/", function(res){
+
+					expect(res).to.exist;
+					expect(res.status).to.equal(200);
+					done();	
+				})	
 			})
 		});
 	});
 	
 	describe('GET /data', function(){
-		it("responds with a HTTP header containing data", function(){
-			http.get("http://localhost:3000/data", function(res){
+		it("responds with a HTTP header containing data", function(done){
+			http.get("http://localhost:3000/data/", function(res){
+
 				expect(res).to.exist;
-				expect(res.status).to.equal(200)
+				res.on("data", function(chunk){
+					//console.log(chunk.toString())
+			
+					done();
+				})
 			})
 			//console.log(d);
 		})
