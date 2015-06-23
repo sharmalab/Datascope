c3ef0496ca88f63035c8c6334a5c532d13981640
Ganesh Iyer <lastlegion@gmail.com>
2015-06-23 21:09:06 +0530

    support for JOIN over multiple data sources
    Former-commit-id: 82bc9293e2508b92d77a7eeaa935ac5d507c93d2

--- modules/dataSource.js
+++ modules/dataSource.js
@@ -3,7 +3,8 @@ var Validator = require('jsonschema').Validator;
 var schemaValidator = new Validator();
 var fs = require("fs");
 var loadDataSource = require("./loadDataSource");
-
+var async = require("async");
+var extend = require("extend");
 var dataSource = (function(){
     var dataSourceConfig = {},
         dataSources = [],
@@ -13,21 +14,23 @@ var dataSource = (function(){
         _loadDataSourceConfig,
         attributes = {},
         dataSourceSchema,
-        validation;
+        validation,
+        keys = [];
     
-
+ 
     var _init = function (){
-
-
-
         for(var i in dataSourcesConfig){
           source = dataSourcesConfig[i];
           dataSources.push(source);
+          /*
           for(var j in source.attributes){
             attribute = source.attributes[j];
             attributes[attribute] = true;
           }
+          */
+          //console.log(dataSources)
         }
+
     };
     var _loadDataSourceConfig = function (path){
         dataSourceConfigPath = path || dataSourceConfigPath;
@@ -35,8 +38,87 @@ var dataSource = (function(){
         dataSourcesConfig = JSON.parse(dataSourcesConfig);   
     };
 
+    var _loadDataSources = function(dataSources, callback){
+        var loadFunctionArray = [];
+        console.log(callback)
+        if(dataSources.length > 1){
+            for(var i=0; i < dataSources.length; i++) {
+                console.log(i);
+                var source = function(){
+                    var dataSourceConfig = dataSources[i];
+
+                    var lds = function(cb) {
+                        loadDataSource(dataSourceConfig, function(data){
+                            cb(null, data);
+                        });
+                    }
+                    return lds;
+                }();
+                keys.push(dataSources[i].key)
+                loadFunctionArray.push(source);
+            }
+
+            async.parallel(loadFunctionArray, function(err, results){
+
+                /*Merge Logic Here*/
+
+                var merged = _merge(results);
+                
+                callback(merged);
+            });
+
+        }
+
+
+    }
+    var _mergetwo = function(data1, data2){
+        var merged = [];
+
+        for(var i=0; i<data1.length; i++){
+            var row1 = data1[i];
+            for(var j=0; j<data2.length; j++){
+                var row2 = data2[j];
+                if(row1[keys[0]] == row2[keys[0]]){
+                    var rowM = extend({}, row1, row2);
+                        merged.push(rowM);
+                }
+            }
+        }
+        var flat = [];
+        merged = merged.concat.apply(flat, merged);
+        return merged;
+    }
+    var _merge = function(results){
+
+            var merged = results.map(function(data, id){
+
+                if(id < results.length-1){
+                    return _mergetwo(results[id], results[id+1]);
+                }
+            })
+            var merged=  merged.filter(function(n){ return n != undefined }); 
+            //console.log("..")
+            console.log(merged[0])
+            return merged[0];
+    }
+
     var _loadData = function(callback){
-        loadDataSource(dataSources, callback);
+        console.log("...")
+        //Load data from sources
+        if(dataSources.length > 1){
+            _loadDataSources(dataSources, callback);    
+        }
+        else {
+            loadDataSource(dataSources[0], function(data){
+                callback(data);
+            })
+                 
+        }
+        //Merge them
+        //_mergeDataSources(dataSources);
+
+
+        //loadDataSource(dataSources, callback);
 
     }
 
