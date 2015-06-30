a4ab320fc5842044455e8a17c20e716342ce8f8a
Ganesh Iyer <lastlegion@gmail.com>
2015-06-30 18:12:28 +0530

    testing joins

--- test/dataSource.js
+++ test/dataSource.js
@@ -4,14 +4,15 @@ var http = require("http");
 var request = require("superagent");
 var expect = require("expect.js");
 
-var dataSource = require("../modules/dataSource");
 
 describe("dataSource", function() {
-
+/*
     describe("init", function() {
 
 
         it("Reads the config files and processes it", function() {
+
+            var dataSource = require("../modules/dataSource");
             console.log("dataSource")
             dataSource.init("./examples/titanicTable/config/dataSource.json");
             var expected_attrbiutes = { 
@@ -33,5 +34,75 @@ describe("dataSource", function() {
             (JSON.stringify(dataSource.getAttributes())).should.be.exactly(JSON.stringify(expected_attrbiutes));
       
         }); 
+    });
+
+*/
+    describe("join", function(){
+      /*
+      it("should return the join of multiple data sources", function(done){
+
+        var dataSource = require("../modules/dataSource");
+        var ds = dataSource.init("./examples/joinDemo/config/dataSource.json");
+        //console.log(ds);  
+        dataSource.loadData(function(data){
+          console.log("...")
+          console.log(data);
+          done();
+        });
+
+      });
+
+      it("should work on tcia data", function(done){
+        var dataSource = require ("../modules/dataSource");
+        var ds = dataSource.init("./examples/joinDemo/config/dataSource.json");
+        dataSource.loadData(function(data){
+
+        })
+      })
+      /*
+      it("should work on TCIA data", function(done){
+
+        var dataSource = require("../modules/dataSource");
+        var ds = dataSource.init("./examples/joinDemo/config/dataSource.json");
+        //console.log(ds);  
+        dataSource.loadData(function(data){
+          console.log("...")
+          console.log(data);
+          done();
+        });
+
+      });
+    */
+
+      it("should work on tcia data", function(done){
+        this.timeout(10000);
+        var dataSource = require ("../modules/dataSource");
+        var anyToJSON = require("anytojson");
+        var ds = dataSource.init("./examples/TCIAJoin2/config/dataSource.json");
+        
+        //var dataSource2 = require("../modules/dataSource");
+        //var d2 = dataSource2.init("./examples/TCIAJoin2/config/dataSource2.json");
+
+        //d1.init("./examples/TCIAJoin2/config/dataSource.json");
+        //d2.init("./examples/TCIAJoin2/config/dataSource2.json");
+
+        dataSource.loadData(function(data){
+            console.log(data.length);
+            var opt = {"path": "./examples/TCIAJoin2/data/CombinedData.csv"};
+            anyToJSON.csv(opt, function(data2){
+              console.log(data2.length)
+              done();
+            })
+            
+        });
+        
+        /*
+          dataSource2.loadData(function(data2){
+            console.log(data2.length)
+            done();  
+          })
+      
+        */
+      })
     }); 
 });
