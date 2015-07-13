c6e49d8cd25c4ae6f89904dd39fbd302bf3050bf
Ganesh Iyer <lastlegion@gmail.com>
2015-07-13 23:00:53 +0530

    added naviagtion to ImageGrid

--- public/javascripts/components/Visualizations/ImageGrid.jsx
+++ public/javascripts/components/Visualizations/ImageGrid.jsx
@@ -1,124 +1,118 @@
 
+var addons = require("react/addons");
+var ReactCSSTransitionGroup = addons.CSSTransitionGroup;
+
 var ImageGrid = React.createClass({
-    componentDidMount: function(){
+
+    componentWillMount: function(){
+        var self =this;
+        var currData = this.props.currData;
+
+        console.log(currData);
+        self.setState({gridState: 0, currData: currData, images: currData})
+    },
+    onPrev: function(e){
         var self = this;
-        var $visualization = d3.select("#imageGrid");
+        e.preventDefault();
+        console.log(this.state.gridState);
+        var gridState = this.state.gridState;
+        gridState--;
+        console.log(gridState);
+        $.get("/imageGrid/next?state="+gridState, function(data){
 
-        $activeRecords = $visualization.append("div")
-            .attr("id", "activeRecords");
-            /*
-        $activeRecords.append("div")
-            .attr("id", "nActive")
-            .text(filteredData["imageGrid"]);
-        $activeRecords.append("div")
-            .attr("id", "nSize")
-            .text(" of "+filteredData["imageGrid"]["size"] + " selected")
-            */  
+            console.log("..")
+            console.log(data);
+            self.setState({
+                gridState: gridState,
+                images: data
+            });
+        });
 
+    }, 
+    onNext: function(e){
+        var self = this;
+        e.preventDefault();
+        console.log(this.state.gridState);
+        var gridState = this.state.gridState;
+        gridState++;
+        console.log(gridState);
+        $.get("/imageGrid/next?state="+gridState, function(data){
 
-        var $grid = $visualization.append("table")
-                        .attr("id", "grid");
-        var $tbody = $grid.append("tbody");
-
-        //var rawData = filteredData["visualization"];
-        var rawData = [];
-        //var filteredData = AppStore.getData();
-        console.log("grid...")
-        console.log(self.props);
-        var filteredData = self.props.currData;
-
-        for(var a in filteredData["imageGrid"]){
-            var d = filteredData["imageGrid"][a];
-            for(var obj in d){
-                var o = d[obj];
-                var i = o["image"];
-                rawData.push(i);
-            }
-        }
-        var gridData = [];
-        while(rawData.length){
-            gridData.push(rawData.splice(0,8));
-        }
-
-        var rows = $tbody.selectAll("tr")
-        .data(gridData)
-        .enter()
-        .append("tr")
-        var cells = rows.selectAll("td")
-        .style({"width": "40px"})
-        .data(function(d) {
-            return d3.values(d);
-        })
-        .enter()
-        .append("td")
-        .html(function(d) {
-            var img = "<img style='border: 1px solid #fff' width='100' src='"+d+"' />"
-            return img;
+            console.log("..")
+            console.log(data);
+            self.setState({
+                gridState: gridState,
+                images: data
+            });
         });
 
     },
-    componentDidUpdate: function(){
-        var self = this;
-        var $visualization = d3.select("#imageGrid");
-        $visualization.html("")
-        $activeRecords = $visualization.append("div")
-            .attr("id", "activeRecords");
-            /*
-        $activeRecords.append("div")
-            .attr("id", "nActive")
-            .text(filteredData["imageGrid"]);
-        $activeRecords.append("div")
-            .attr("id", "nSize")
-            .text(" of "+filteredData["imageGrid"]["size"] + " selected")
-            */  
-
-
-        var $grid = $visualization.append("table")
-                        .attr("id", "grid");
-        var $tbody = $grid.append("tbody");
-
-        //var rawData = filteredData["visualization"];
-        var rawData = [];
-        //var filteredData = AppStore.getData();
-        console.log("grid...")
-        console.log(self.props);
-        var filteredData = self.props.currData;
-
-        for(var a in filteredData["imageGrid"]){
-            var d = filteredData["imageGrid"][a];
-            for(var obj in d){
-                var o = d[obj];
-                var i = o["image"];
-                rawData.push(i);
-            }
-        }
-        var gridData = [];
-        while(rawData.length){
-            gridData.push(rawData.splice(0,8));
-        }
-
-        var rows = $tbody.selectAll("tr")
-        .data(gridData)
-        .enter()
-        .append("tr")
-        var cells = rows.selectAll("td")
-        .style({"width": "40px"})
-        .data(function(d) {
-            return d3.values(d);
-        })
-        .enter()
-        .append("td")
-        .html(function(d) {
-            var img = "<img style='border: 1px solid #fff' width='100' src='"+d+"' />"
-            return img;
-        });
-    },  
-    
     render: function(){
-        return(
-            <div id="imageGrid" ></div>
-        );
+        var self =this;
+        var currData = this.props.currData;
+        var data = this.state.images;
+        console.log(data);
+
+        var images = data["imageGrid"]["values"];
+        var finalState = data["imageGrid"]["finalState"];
+        console.log(images)
+        console.log(this.state);
+        console.log(finalState)
+        var gridState = this.state.gridState;   
+        var Img = images.map(function(d){
+            var image = d["image"];
+
+            return (
+                
+
+                    <img src={image} width="60px" height="60px" style={{margin: "2px"}}/>
+        
+            );
+
+
+        });
+        if(gridState == 0){
+            return(
+
+                <div id="imageGrid" >
+                    <div id="imageGridImages">
+                            {Img}
+                    </div>
+                   <div id="imageGridPagination">
+                        <a href="#" className="next" onClick={this.onNext} >Next</a>
+                    </div>
+                </div>
+
+            );           
+        } else if(gridState == finalState) {
+
+            return(
+                <div id="imageGrid" >
+                    <div id="imageGridImages">
+                            {Img}
+                    </div>
+                   <div id="imageGridPagination">
+                        <a href="#" className="prev" onClick={this.onPrev}>Prev</a>
+                    </div>
+                </div>
+            );
+        } else {
+            return(
+                <div id="imageGrid" >
+                    <div id="imageGridImages">
+                            {Img}
+                    </div>
+                   <div id="imageGridPagination">
+                        <a href="#" className="prev" onClick={this.onPrev}>Prev</a>
+                        <a href="#" className="next" onClick={this.onNext}>Next</a>
+                    </div>
+                </div>
+            );
+        }
+
     }
 });
 
 module.exports = ImageGrid;
+
+
