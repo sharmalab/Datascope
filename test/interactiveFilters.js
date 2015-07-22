15f74fad0eb8e7e770c1ed88f31c312e74746aed
Ganesh Iyer <lastlegion@gmail.com>
2015-07-22 23:14:26 +0530

    added tests
    Former-commit-id: a1efe9d1d09eeda939d865ff1e561b3c156cb81b

--- test/interactiveFilters.js
+++ test/interactiveFilters.js
@@ -20,35 +20,38 @@ describe("interactive Filters", function() {
 
           //console.log(data);
           interactiveFilters.applyCrossfilter(data);
-          var truth = [[ { key: 2, value: 1 },
-                        { key: 3, value: 1 },
-                        { key: 4, value: 1 },
-                        { key: 1, value: 1 } ],
-                      [ { key: 'dfg', value: 1 },
-                        { key: 'qwe', value: 1 },
-                        { key: 'zxc', value: 1 },
-                        { key: 'asd', value: 1 } ],
-                      [ { key: 'banana', value: 1 },
-                        { key: 'mango', value: 1 },
-                        { key: 'orange', value: 1 },
-                        { key: 'apple', value: 1 } ],
-                      [ { key: 10, value: 1 },
-                        { key: 12, value: 1 },
-                        { key: 20, value: 1 },
-                        { key: 3, value: 1 } ]];
+          var truth = [ [ { key: '2', value: 1 },
+                          { key: '3', value: 1 },
+                          { key: '4', value: 1 },
+                          { key: '1', value: 1 } ],
+                        [ { key: 'dfg', value: 1 },
+                          { key: 'qwe', value: 1 },
+                          { key: 'zxc', value: 1 },
+                          { key: 'asd', value: 1 } ],
+                        [ { key: 'banana', value: 1 },
+                          { key: 'mango', value: 1 },
+                          { key: 'orange', value: 1 },
+                          { key: 'apple', value: 1 } ],
+                        [ { key: '12', value: 1 },
+                          { key: '20', value: 1 },
+                          { key: '3', value: 1 },
+                          { key: '10', value: 1 }
+                     ] ];
 
 
           var groups = interactiveFilters.getGroups();
 
-          //console.log(groups)
           var results = []
           for(var g in groups){
             var group  = groups[g];
             results.push(group.top(Infinity));
             //console.log(group.top(Infinity));
           }
+          (results).should.be.eql(truth)
+          //console.log("results...")
           //console.log(results)
-          (results).should.be.eql(truth);
+          //console.log(results)
+          //(results).should.be.eql(truth);
           //console.log(interactiveFilters.getGroups()["A"].top(Infinity));
           done();  
         });
