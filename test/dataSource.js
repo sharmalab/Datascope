0e881d2a9e9c4f19cf0c10b5348ba51b3e2a421d
Ganesh Iyer <lastlegion@gmail.com>
2015-07-22 17:31:24 +0530

    added tests

--- test/dataSource.js
+++ test/dataSource.js
@@ -67,13 +67,98 @@ describe("dataSource", function() {
         //console.log(ds);  
         dataSource.loadData(function(data){
           console.log("...")
-          console.log(data);
+z          console.log(data);
           done();
         });
 
       });
     */
+    
+      it("should work on single source dummy data", function(done){
 
+        var dataSource = require("../modules/dataSource");
+        var ds = dataSource.init("./examples/newDataSourceConfig/config/dataSource2.json");
+        //console.log(ds);
+        dataSource.loadData(function(data){
+          //console.log(data);
+          var truth = [
+            {
+              "A":"1",
+              "B":"asd",
+              "C":"apple",
+              "D":"20"
+            },
+            {
+              "A":"2",
+              "B":"zxc",
+              "C":"orange",
+              "D":"10"
+            },
+            {
+              "A":"3",
+              "B":"qwe",
+              "C":"banana",
+              "D":"3"
+            },
+            {
+              "A":"4",
+              "B":"dfg",
+              "C":"mango",
+              "D":"12"
+            }
+          ];
+          (data).should.be.eql(truth);
+
+          done();
+        })
+
+      });
+      
+
+      it("should work on multi source dummy data", function(done){
+
+        var dataSource = require("../modules/dataSource");
+        var ds = dataSource.init("./examples/newDataSourceConfig/config/dataSource.json");
+        //console.log(ds);
+        //console.log("multi")
+        dataSource.loadData(function(data){
+          //console.log("...")
+
+          //console.log(data);
+
+          var truth = [
+            {
+              "A":"1",
+              "B":"asd",
+              "C":"apple",
+              "D":"20"
+            },
+            {
+              "A":"2",
+              "B":"zxc",
+              "C":"orange",
+              "D":"10"
+            },
+            {
+              "A":"3",
+              "B":"qwe",
+              "C":"banana",
+              "D":"3"
+            },
+            {
+              "A":"4",
+              "B":"dfg",
+              "C":"mango",
+              "D":"12"
+            }
+          ];
+          (data).should.be.eql(truth);
+
+          done();
+        })
+
+      });
+      /*
       it("should work on tcia data", function(done){
         this.timeout(10000);
         var dataSource = require ("../modules/dataSource");
@@ -102,7 +187,9 @@ describe("dataSource", function() {
             done();  
           })
       
-        */
       })
+
     }); 
+        */
+    });
 });
