8a96d1f6897234d5a5fc8d1a060ab76ba1078d01
Ganesh Iyer <lastlegion@gmail.com>
2015-12-11 15:34:26 -0500

    eslint fixes, added splom filters

--- modules/visualization.js
+++ modules/visualization.js
@@ -1,5 +1,5 @@
 //JSON Schema validation
-var Validator = require('jsonschema').Validator;
+var Validator = require("jsonschema").Validator;
 var schemaValidator = new Validator();
 var fs = require("fs");
 
@@ -11,45 +11,60 @@ var visualization = (function(){
 
     var visualizationConfig = {},
         ndx = {},
-        visualizations = [] //array containing information about all the visualizations
+        visualizations = [], //array containing information about all the visualizations
         visualizationConfigPath = "public/config/visualization.json";
 
-
+    var splomFilters = function(ndx, visualization){
+        var attributes=visualization.attributes;
+        for(var i=0; i<attributes.length; i++){
+            for(var j=1; j<attributes.length; j++){
+                var xAttr = attributes[i].attributeName;
+                var yAttr = attributes[j].attributeName;
+                var dimension = ndx.dimension(function(d){
+                    return( [ +d.xAttr, +d.yAttr ]);
+                });
+                var group = dimension.group();
+                interactiveFilters.addDimension(xAttr + "-"+ yAttr, dimension);
+                interactiveFilters.addGroup(xAttr + "-" + yAttr, group);
+            }
+        }
+        
+    };
     var imageGridFilters = function (ndx, visualization){
-      var dimension;
-      for(var attr in visualization.attributes){
-        var attribute = visualization.attributes[attr];
-        if(attribute.type == "image"){  
-          dimension = attribute.attributeName;
-          dimension = ndx.dimension(function(d){
-            return d[attribute.attributeName];        
-          });
+        var dimension;
+        for(var attr in visualization.attributes){
+            var attribute = visualization.attributes[attr];
+            if(attribute.type == "image"){  
+                dimension = attribute.attributeName;
+                dimension = ndx.dimension(function(d){
+                    return d[attribute.attributeName];        
+                });
+            }
+
         }
 
-      }
-
-      interactiveFilters.addDimension("imageGrid", dimension);
-      interactiveFilters.addGroup("imageGrid", dimension.group());
-    }
+        interactiveFilters.addDimension("imageGrid", dimension);
+        interactiveFilters.addGroup("imageGrid", dimension.group());
+    };
 
     var heatMapFilters = function(ndx, visualization){
-      var xAttr, yAttr;
-      for(var attr in visualization.attributes){
-        var attribute = visualization.attributes[attr];
-        if(attribute.type == "x"){  
-          xAttr = attribute.attributeName;
-        } else if(attribute.type == "y"){
-          yAttr = attribute.attributeName;
+        var xAttr, yAttr;
+        for(var attr in visualization.attributes){
+            var attribute = visualization.attributes[attr];
+            if(attribute.type == "x"){  
+                xAttr = attribute.attributeName;
+            } else if(attribute.type == "y"){
+                yAttr = attribute.attributeName;
+            }
         }
-      }
-      var dimension = ndx.dimension(function(d) {
 
-        return ([+d[xAttr]*1, +d[yAttr]*1]);
-      });
-      var group = dimension.group();
-      interactiveFilters.addDimension("heatMap", dimension);
-      interactiveFilters.addGroup("heatMap", dimension.group());
-    }
+        var dimension = ndx.dimension(function(d) {
+            return ([+d[xAttr]*1, +d[yAttr]*1]);
+        });
+        //var group = dimension.group();
+        interactiveFilters.addDimension("heatMap", dimension);
+        interactiveFilters.addGroup("heatMap", dimension.group());
+    };
     var bubbleChartFilters = function(){
         var xAttr;
         var yAttr;
@@ -57,50 +72,52 @@ var visualization = (function(){
         var colorAttr;
         var dimension;
         for(var attr in visualization.attributes){
-          var attribute = visualization.attributes[attr];
-          if(attribute.dimension){  
-            dimension = attribute.name;
-            dimension["visualization"] = ndx.dimension(function(d){
-              return d[attribute.name];        
-            });
-          }
-          if(attribute.type == "x"){
-            xAttr = attribute.name;
-          }
-          if(attribute.type == "y"){
-            yAttr = attribute.name;
-          }
-          if(attribute.type == "r"){
-            rAttr = attribute.name;
-          }
-          if(attribute.type == "color"){
-            colorAttr = attribute.name;
-          }
+            var attribute = visualization.attributes[attr];
+            if(attribute.dimension){  
+                dimension = attribute.name;
+                dimension["visualization"] = ndx.dimension(function(d){
+                    return d[attribute.name];        
+                });
+            }
+            if(attribute.type == "x"){
+                xAttr = attribute.name;
+            }
+            if(attribute.type == "y"){
+                yAttr = attribute.name;
+            }
+            if(attribute.type == "r"){
+                rAttr = attribute.name;
+            }
+            if(attribute.type == "color"){
+                colorAttr = attribute.name;
+            }
         }
+        var dimensions = {};
+        var groups = {};
         groups["visualization"] = dimensions[dimension].group().reduce(
-          function(p,v){
-            p[xAttr] += v[xAttr];
-            p[yAttr] += v[yAttr];
-            p[rAttr] += v[rAttr];
-            p[colorAttr] += v[colorAttr];
-            return p;
-          },
-          function(p,v){
-            p[xAttr] -= v[xAttr];
-            p[yAttr] -= v[yAttr];
-            p[rAttr] -= v[rAttr];
-            p[colorAttr] -= v[colorAttr];
-            return p;
-          },
-          function(){
-            var obj = {};
-            obj[xAttr] = 0;
-            obj[yAttr] = 0;
-            obj[rAttr] = 0;
-            obj[colorAttr] = 0;
-            return obj;
-          }
-        );  
+            function(p,v){
+                p[xAttr] += v[xAttr];
+                p[yAttr] += v[yAttr];
+                p[rAttr] += v[rAttr];
+                p[colorAttr] += v[colorAttr];
+                return p;
+            },
+            function(p,v){
+                p[xAttr] -= v[xAttr];
+                p[yAttr] -= v[yAttr];
+                p[rAttr] -= v[rAttr];
+                p[colorAttr] -= v[colorAttr];
+                return p;
+            },
+            function(){
+                var obj = {};
+                obj[xAttr] = 0;
+                obj[yAttr] = 0;
+                obj[rAttr] = 0;
+                obj[colorAttr] = 0;
+                return obj;
+            }
+        ); 
     };
 
 
@@ -116,36 +133,37 @@ var visualization = (function(){
         var config  = _loadConfig(path);
         visualizations = [];
         for (var i in config) {
-          visualizations.push(config[i]);
+            visualizations.push(config[i]);
 
         }
 
         //_visualizationFilters();
         return config;
-    }
+    };
 
     var _applyCrossfilter = function(){
 
-      var ndx = interactiveFilters.getndx();
+        var ndx = interactiveFilters.getndx();
       //console.log(ndx);
 
-      for(var i in visualizations){
-        var vis = visualizations[i];
+        for(var i in visualizations){
+            var vis = visualizations[i];
 
-        switch(vis.visualizationType){
-          case "imageGrid":
-            imageGridFilters(ndx,vis);
-            break;
-          case "heatMap":
-            heatMapFilters(ndx,vis);
-            break;
+            switch(vis.visualizationType){
+            case "imageGrid":
+                imageGridFilters(ndx,vis);
+                break;
+            case "heatMap":
+                heatMapFilters(ndx,vis);
+                break;
+            case "splom":
+                splomFilters(ndx,vis);
+                break;
+            }
         }
 
 
-      }
-
-
-    }
+    };
 
     return{
         init: _init,
@@ -153,30 +171,26 @@ var visualization = (function(){
         //Returns an array of attributes corresponding to the visualization
         getAttributes: function(visualizationType){
 
-          for(var i in visualizations){
-            var visualization = visualizations[i];
-            if(visualization.visualizationType == visualizationType){
-
-              return visualization.attributes;
-
+            for(var i in visualizations){
+                var visualization = visualizations[i];
+                if(visualization.visualizationType == visualizationType){
+                    return visualization.attributes;
+                }
             }
-          }
         },
         getVisualizationType: function(){
             return visualizationConfig.type;
         }, 
         hasVisualization: function(visualizationType){
-          for(var i in visualizations){
-            var visualization = visualizations[i];
-            if(visualization.visualizationType == visualizationType){
-
-              return true;
+            for(var i in visualizations){
+                var visualization = visualizations[i];
+                if(visualization.visualizationType == visualizationType){
+                    return true;
+                }
             }
-
-          }
-          return false;
+            return false;
         }
-    }
+    };
 })();
  
 module.exports = visualization;
