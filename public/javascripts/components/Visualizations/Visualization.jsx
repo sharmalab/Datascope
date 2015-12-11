3ead0ebe5bf72b847dc7675e1b3a3b46d96b83cf
Ganesh Iyer <lastlegion@gmail.com>
2015-12-11 15:33:34 -0500

    fixed lint errors, added splom placeholder

--- public/javascripts/components/Visualizations/Visualization.jsx
+++ public/javascripts/components/Visualizations/Visualization.jsx
@@ -1,41 +1,46 @@
 var React =require("react");
 var DataTable = require("./DataTable.jsx"),
     HeatMap = require("./HeatMap.jsx"),
-    ImageGrid = require("./ImageGrid.jsx");
-
+    ImageGrid = require("./ImageGrid.jsx"),
+    Splom = require("./Splom.jsx");
 
 var Visualization = React.createClass({
     render: function(){
         var visType = this.props.config.visualizationType;
-        var self = this;
+        //var self = this;
         console.log("Visualization");
         console.log(visType);
+        
         switch(visType) {
-            case "dataTable":
-    
-                return(
-                    <DataTable config={this.props.config} currData = {this.props.currData} />
-                );
-                break;
-            case "bubbleChart":
-                return(
-                    <div></div>
-                );
-                break;
-            case "heatMap":
-                return(
-                    <HeatMap config={this.props.config} currData = {this.props.currData} />
-                );
-                break;
-            case "imageGrid":
-                return(
-                    <ImageGrid config={this.props.config} debug={this.props.debug} currData = {this.props.currData} />
-                );
-                break;
-            default:
-                return(
-                    <div></div>
-                );
+        
+        case "dataTable": 
+            return(
+                <DataTable config={this.props.config} currData = {this.props.currData} />
+            );
+            //break;
+        case "bubbleChart":
+            return(
+                <div></div>
+            );
+            //break;
+        case "heatMap":
+            return(
+                <HeatMap config={this.props.config} currData = {this.props.currData} />
+            );
+            //break;
+        case "imageGrid":
+            return(
+                <ImageGrid config={this.props.config} debug={this.props.debug} currData = {this.props.currData} />
+            );
+            //break;
+        case "SPLOM": 
+            return(
+                <Splom config={this.props.config} />
+            );
+        default:
+            return(
+                <div></div>
+            );
         }         
           
     }
