f403eb8bd069b4b75e828e3a7d99136bffac31cf
Ganesh Iyer <lastlegion@gmail.com>
2015-12-18 13:04:12 -0500

    removed downloads button

--- public/javascripts/src/components/Visualizations.jsx
+++ public/javascripts/src/components/Visualizations.jsx
@@ -94,7 +94,7 @@ var Visualizations = React.createClass({
 
             return(
                 <div id="visualization" className="col-sm-7 col-md-7 col-lg-8">
-                    <OptionsBar currData={self.props.currData} />
+                   
                     <Tabs defaultActiveKey={1}>
                         {visualizations}
                     </Tabs>
