42031094ae610e8316cd8eb867244c8a87e70052
Ganesh Iyer <lastlegion@gmail.com>
2015-12-18 13:26:13 -0500

    moved download button

--- public/javascripts/src/components/Navbar.jsx
+++ public/javascripts/src/components/Navbar.jsx
@@ -8,7 +8,12 @@ var NavBar = React.createClass({
             <div className='navbar navbar-inverse navbar-fixed-top' id='header' role='navigation'>
                 <div className='navbar-header'>
                     <div className="navbar-brand">Data Explorer</div>
+                        
                 </div>
+
+                    <ul className="nav navbar-nav navbar-right navbar-options">
+                        <li><a href="#">Download</a></li>
+                    </ul>
             </div>
         );
     }
