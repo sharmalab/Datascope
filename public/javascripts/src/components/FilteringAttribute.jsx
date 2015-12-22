e751cff18f479b832a5877b6eb9bb0cb1bc57694
Ganesh Iyer <lastlegion@gmail.com>
2015-12-22 17:59:24 -0500

    added icons on filtering attributes

--- public/javascripts/src/components/FilteringAttribute.jsx
+++ public/javascripts/src/components/FilteringAttribute.jsx
@@ -346,7 +346,11 @@ var FilteringAttribute = React.createClass({
         var self = this;
         var divId = "dc-"+this.props.config.attributeName;
         var showChart = self.state.showChart ? {display: "block"} : {display: "none"};
-        console.log(showChart);
+
+        var iconHeight = "20px";
+        var iconWidth = "20px";
+
+        //console.log(showChart);
         //console.log(this.props.currData);
         if(this.props.full === true){
             console.log("FULL!");
@@ -356,7 +360,6 @@ var FilteringAttribute = React.createClass({
                     <div className="chart-wrapper">
                         <div className="chart-title">
                             {self.props.config.attributeName}
-
                         </div>
                         <div>
                             <div className="chart-stage">
@@ -376,8 +379,20 @@ var FilteringAttribute = React.createClass({
             return (
                 <div className="col-md-12" onClick={this.fullView}>
                     <div className="chart-wrapper">
-                        <div className="chart-title" onClick={self.showChart} >
+                        <div className="chart-title" >
                             {self.props.config.attributeName}
+                            <div className="chart-title-icons">
+                                <svg style={{width:iconWidth,height:iconHeight }} viewBox="0 0 24 24" onClick={self.onReset}>
+                                    <path fill="#fff" d="M14.73,20.83L17.58,18L14.73,15.17L16.15,13.76L19,16.57L21.8,13.76L23.22,15.17L20.41,18L23.22,20.83L21.8,22.24L19,19.4L16.15,22.24L14.73,20.83M2,2H20V2H20V4H19.92L14,9.92V22.91L8,16.91V9.91L2.09,4H2V2M10,16.08L12,18.08V9H12.09L17.09,4H4.92L9.92,9H10V16.08Z">
+                                        <title>Remove filter</title>
+                                    </path>
+                                </svg>
+                                <svg  style={{width: iconWidth ,height:iconHeight}} viewBox="0 0 24 24" onClick={self.showChart} >
+                                    <path fill="#fff" d="M20,14H4V10H20">
+                                        <title>Hide attribute</title>
+                                    </path>
+                                </svg>
+                            </div>
                         </div>
                        
                         <div style={showChart}>
@@ -385,7 +400,7 @@ var FilteringAttribute = React.createClass({
                                 <div  id={divId}> </div>
                             </div>
                             <div className="chart-notes">
-                                <button onClick={this.onReset}>Reset</button>
+
                                 <ChartAddons config={this.props.config} data={this.state.currData} chart={this.state.chart}/>
 
                             </div>
