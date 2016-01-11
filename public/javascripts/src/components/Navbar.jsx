df87ed4fe9e51b92f3f99cd5fbb33a1435cfe331
Ganesh Iyer <lastlegion@gmail.com>
2016-01-11 15:02:11 -0500

    ability to specify title from dashboard.json

--- public/javascripts/src/components/Navbar.jsx
+++ public/javascripts/src/components/Navbar.jsx
@@ -1,17 +1,33 @@
 /* global queryFilter */
+/* global d3 */
 var React = require("react");
+var DocumentTitle = require("react-document-title");
 
 var NavBar = React.createClass({
+    getInitialState: function(){
+       return {}; 
+    },  
+    componentDidMount: function() {
+        var self = this;
+        d3.json("config/dashboard.json", function(config){
+            var dashBoardConfig =  config || {};
+            console.log(dashBoardConfig.projectTitle);
+            var title = dashBoardConfig.projectTitle || "DataScope";
+            self.setState({projectTitle: title});
 
+        });
+
+    },
     render: function() {
         
-        var self = this;
-        var filter = queryFilter;
 
+
+        var projectTitle = this.state.projectTitle || "DataScope";
         return(
             <div className='navbar navbar-inverse navbar-fixed-top' id='header' role='navigation'>
+                <DocumentTitle title={"Datascope :: "+ projectTitle}/>
                 <div className='navbar-header'>
-                    <div className="navbar-brand">Data Explorer</div>
+                    <div className="navbar-brand">{projectTitle}</div>
                         
                 </div>
 
