7dce1e99bd7778a2d85a42003bf4e490ca9b6624
Ganesh Iyer <lastlegion@gmail.com>
2016-01-30 14:57:14 -0500

    added links in datatable

--- public/javascripts/src/components/Visualizations/DataTable.jsx
+++ public/javascripts/src/components/Visualizations/DataTable.jsx
@@ -9,12 +9,16 @@ var DataTable = React.createClass({
 
             var columns = [];
             var count=0;
+            var url;
             for(var i in self.props.config.attributes){
                 columns[count] = {};
                 //columns[count]["data"] = self.props.config.attributes[i].name;
                 columns[count]["title"] = self.props.config.attributes[i].label || self.props.config.attributes[i].attributeName;
                 columns[count]["bSearchable"]= false;
                 columns[count]["bSortable"] =false ;
+                if(self.props.config.attributes[i].type){
+                    url = self.props.config.attributes[i];
+                }
                 count++;
             }
             dataTable = $('#vis').DataTable({
@@ -32,10 +36,17 @@ var DataTable = React.createClass({
                 responsive: true
 
             });
+                var url = self.props.config.url;
+
             $("#vis tbody").on("click", "tr", function(){
-                //var url = self.props.config.url || "http://imaging.cci.emory.edu/phone/";
-                //window.open(url); 
+                //var url = self.props.config.url
+                //var id = DataTable.fnGetData(this)[0];
+                console.log(dataTable.row(this).data());
+                var data = dataTable.row(this).data();
+                var url = data[data.length-1];
+                window.open(url); 
             });
+
 
     },
     componentWillReceiveProps: function(){
