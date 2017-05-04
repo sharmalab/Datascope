7394520824e829101eff320d5005f0727cda09f3
Ryan Birmingham <birm@rbirm.us>
2017-05-04 11:49:50 -0400

    test formatting

--- test/dataSource.js
+++ test/dataSource.js
@@ -6,39 +6,39 @@ var expect = require("expect.js");
 
 
 describe("dataSource", function() {
-/*
-    describe("init", function() {
+  /*
+      describe("init", function() {
 
 
-        it("Reads the config files and processes it", function() {
+          it("Reads the config files and processes it", function() {
 
-            var dataSource = require("../modules/dataSource");
-            console.log("dataSource")
-            dataSource.init("./examples/titanicTable/config/dataSource.json");
-            var expected_attrbiutes = {
-              PassengerId: true,
-              Survived: true,
-              Pclass: true,
-              Name: true,
-              Sex: true,
-              Age: true,
-              SibSp: true,
-              Parch: true,
-              Ticket: true,
-              Fare: true,
-              Cabin: true,
-              Embarked: true
-            };
+              var dataSource = require("../modules/dataSource");
+              console.log("dataSource")
+              dataSource.init("./examples/titanicTable/config/dataSource.json");
+              var expected_attrbiutes = {
+                PassengerId: true,
+                Survived: true,
+                Pclass: true,
+                Name: true,
+                Sex: true,
+                Age: true,
+                SibSp: true,
+                Parch: true,
+                Ticket: true,
+                Fare: true,
+                Cabin: true,
+                Embarked: true
+              };
 
 
-            (JSON.stringify(dataSource.getAttributes())).should.be.exactly(JSON.stringify(expected_attrbiutes));
+              (JSON.stringify(dataSource.getAttributes())).should.be.exactly(JSON.stringify(expected_attrbiutes));
 
-        });
-    });
+          });
+      });
 
-*/
-    describe("join", function(){
-      /*
+  */
+  describe("join", function() {
+    /*
       it("should return the join of multiple data sources", function(done){
 
         var dataSource = require("../modules/dataSource");
@@ -74,88 +74,88 @@ z          console.log(data);
       });
     */
 
-      it("should work on single source dummy data", function(done){
+    it("should work on single source dummy data", function(done) {
 
-        var dataSource = require("../modules/dataSource");
-        var ds = dataSource.init("./examples/newDataSourceConfig/config/dataSource2.json");
-        //console.log(ds);
-        dataSource.loadData(function(data){
-          //console.log(data);
-          var truth = [
-            {
-              "A":"1",
-              "B":"asd",
-              "C":"apple",
-              "D":"20"
-            },
-            {
-              "A":"2",
-              "B":"zxc",
-              "C":"orange",
-              "D":"10"
-            },
-            {
-              "A":"3",
-              "B":"qwe",
-              "C":"banana",
-              "D":"3"
-            },
-            {
-              "A":"4",
-              "B":"dfg",
-              "C":"mango",
-              "D":"12"
-            }
-          ];
-          (data).should.be.eql(truth);
-        })
-        done();
+      var dataSource = require("../modules/dataSource");
+      var ds = dataSource.init(
+        "./examples/newDataSourceConfig/config/dataSource2.json");
+      //console.log(ds);
+      dataSource.loadData(function(data) {
+        //console.log(data);
+        var truth = [{
+            "A": "1",
+            "B": "asd",
+            "C": "apple",
+            "D": "20"
+          },
+          {
+            "A": "2",
+            "B": "zxc",
+            "C": "orange",
+            "D": "10"
+          },
+          {
+            "A": "3",
+            "B": "qwe",
+            "C": "banana",
+            "D": "3"
+          },
+          {
+            "A": "4",
+            "B": "dfg",
+            "C": "mango",
+            "D": "12"
+          }
+        ];
+        (data).should.be.eql(truth);
+      })
+      done();
+    });
+
+
+    it("should work on multi source dummy data", function(done) {
+
+      var dataSource = require("../modules/dataSource");
+      var ds = dataSource.init(
+        "./examples/newDataSourceConfig/config/dataSource.json");
+      //console.log(ds);
+      //console.log("multi")
+      dataSource.loadData(function(data) {
+        //console.log("...")
+
+        //console.log(data);
+
+        var truth = [{
+            "A": "1",
+            "B": "asd",
+            "C": "apple",
+            "D": "20"
+          },
+          {
+            "A": "2",
+            "B": "zxc",
+            "C": "orange",
+            "D": "10"
+          },
+          {
+            "A": "3",
+            "B": "qwe",
+            "C": "banana",
+            "D": "3"
+          },
+          {
+            "A": "4",
+            "B": "dfg",
+            "C": "mango",
+            "D": "12"
+          }
+        ];
+        (data).should.be.eql(truth);
       });
+      done();
 
-
-      it("should work on multi source dummy data", function(done){
-
-        var dataSource = require("../modules/dataSource");
-        var ds = dataSource.init("./examples/newDataSourceConfig/config/dataSource.json");
-        //console.log(ds);
-        //console.log("multi")
-        dataSource.loadData(function(data){
-          //console.log("...")
-
-          //console.log(data);
-
-          var truth = [
-            {
-              "A":"1",
-              "B":"asd",
-              "C":"apple",
-              "D":"20"
-            },
-            {
-              "A":"2",
-              "B":"zxc",
-              "C":"orange",
-              "D":"10"
-            },
-            {
-              "A":"3",
-              "B":"qwe",
-              "C":"banana",
-              "D":"3"
-            },
-            {
-              "A":"4",
-              "B":"dfg",
-              "C":"mango",
-              "D":"12"
-            }
-          ];
-          (data).should.be.eql(truth);
-        });
-        done();
-
-      });
-      /*
+    });
+    /*
       it("should work on tcia data", function(done){
         this.timeout(10000);
         var dataSource = require ("../modules/dataSource");
@@ -188,5 +188,5 @@ z          console.log(data);
 
     });
         */
-    });
+  });
 });
